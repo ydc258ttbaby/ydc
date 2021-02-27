d2bf3584c92f49598a04d0fd48179888a8369740
ydc258ttbaby <2625736149@qq.com>
2021-02-27 21:17:41 +0800

    日常提交

--- Code/C++/HelloWorld/HelloWorld/Main.cpp
+++ Code/C++/HelloWorld/HelloWorld/Main.cpp
@@ -1,20 +1,20 @@
 #include<iostream>
-//void PrintName(std::string name) // 可接受左值和右值
-//{
-//	std::cout<<name<<std::endl;
-//}
-//void PrintName(std::string& name) // 只接受左值引用，不接受右值
-//{
-//	std::cout << name << std::endl;
-//}
+void PrintName(std::string name) // 可接受左值和右值
+{
+	std::cout<<name<<std::endl;
+}
+void PrintName(std::string& name) // 只接受左值引用，不接受右值
+{
+	std::cout << name << std::endl;
+}
 void PrintName(const std::string& name) // 接受左值和右值，把右值当作const lvalue&
 {
 	std::cout << name << std::endl;
 }
-//void PrintName(std::string&& name) // 接受右值引用
-//{
-//	std::cout << name << std::endl;
-//}
+void PrintName(std::string&& name) // 接受右值引用
+{
+	std::cout << name << std::endl;
+}
 int main()
 {
 	std::string firstName = "yang";
