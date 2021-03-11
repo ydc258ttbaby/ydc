17ac36dee2707e7dcad647bc90b1db3625ee7725
ydc258ttbaby <2625736149@qq.com>
2021-03-11 12:12:38 +0800

    添加 Chapter2-渲染管线.docx

--- Code/C++/HelloWorld/HelloWorld/Main.cpp
+++ Code/C++/HelloWorld/HelloWorld/Main.cpp
@@ -1,26 +1,11 @@
 #include<iostream>
-void PrintName(std::string name) // 可接受左值和右值
-{
-	std::cout<<name<<std::endl;
-}
-void PrintName(std::string& name) // 只接受左值引用，不接受右值
-{
-	std::cout << name << std::endl;
-}
-void PrintName(const std::string& name) // 接受左值和右值，把右值当作const lvalue&
-{
-	std::cout << name << std::endl;
-}
-void PrintName(std::string&& name) // 接受右值引用
-{
-	std::cout << name << std::endl;
-}
+#include<string>
+
 int main()
 {
-	std::string firstName = "yang";
-	std::string lastName = "dingchao";
-	std::string fullName = firstName + lastName;
-	PrintName(fullName);
-	PrintName(firstName+lastName);
+	std::string s("abcdefg");
+	auto it = s.begin();
+	s.erase(it, it+5);
+	std::cout<<s<<std::endl;
 	std::cin.get();
 }
